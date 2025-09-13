336475a39bf36a99d706ec747f2d9bdf0f98c5ba
Apig Aramian <apigaramian@Apigs-MacBook-Pro.local>
2025-09-13 19:55:59 +0400

    fixed some one to someone

--- Test/src/Main.java
+++ Test/src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello World!");
-        System.out.println("Some one added a thing!! use git pull to see it!");
+        System.out.println("Someone added a thing!! use git pull to see it!");
         
     }
 }
